f55618ebed5817e27e8716c0c226e21231cbbd1b
Skander Jeddi <skander.jeddi2@gmail.com>
2021-06-15 14:52:59 +0200

    Clearer code and variable naming

--- pycrypt.py
+++ pycrypt.py
@@ -18,22 +18,40 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
 
+'''
+pyAesCrypt is a Python 3 file-encryption module and script that uses AES256-CBC to encrypt/decrypt files and binary streams.
+PyPI page: https://pypi.org/project/pyAesCrypt/
+GitHub page: https://github.com/marcobellaccini/pyAesCrypt
+'''
+import pyAesCrypt as aes
+
+# Path & IO related imports
 import os
 from os import path, walk
 from pathlib import Path
 import ntpath
-import sys
+
+# Random file name generator, should always be unique
 import uuid
-import pyAesCrypt as aes
+
+from sys import argv
 
 def main():
-    if len(sys.argv) != 4:
+    if len(argv) != 4:
         usage()
     else:
-        mode, password, filepath = sys.argv[1].lower(), sys.argv[2], sys.argv[3]
+        mode, password, file_path = argv[1].lower(), argv[2], os.path.abspath(argv[3])
         if mode == 'e' or mode == 'd':
-            absp = os.path.abspath(filepath)
-            process(mode.lower(), password, absp)
+            try:
+                process(mode, password, file_path)
+            except FileNotFoundError:
+                print(f'\n[!] Couldn\'t find file {file_path}, exiting...')
+                exit(1)
+            except ValueError:
+                print(f'\n[!] An unspecified exception was thrown while trying to process {file_path}.')
+                print(f'[!] This often means that the input file could not be located. Check your path and try again.')
+                print('[!] Exiting...')
+                exit(1)
         else:
             print(f'[!] Unrecognized operation "{mode}"')
             usage()
@@ -46,41 +64,41 @@ def path_leaf(path):
     head, tail = ntpath.split(path)
     return tail or ntpath.basename(head)
 
-def process_file(mode, password, filename, dir='.', newfilename=None, delete=True):
-    if newfilename == None:
-        newfilename = str(uuid.uuid4())
-    basefilepath = f'{dir}/{filename}'
-    fname, ext = path.splitext(basefilepath)
-    resultfilepath = f'{dir}/{newfilename}{ext}'
-    basefilename, resultfilename = path_leaf(basefilepath), path_leaf(resultfilepath)
+def process_file(mode, password, file_absolute_path, delete=True):
+    file_path_object = Path(file_absolute_path)
+    file_parent = file_path_object.parent.absolute()
+    random_uuid = str(uuid.uuid4())
+    input_file_extension = path.splitext(file_absolute_path)[1]
+    output_file_path = f'{file_parent}/{random_uuid}{input_file_extension}'
+    input_file_name, output_file_name = path_leaf(file_absolute_path), path_leaf(output_file_path)
     if mode == 'e':
-        print(f'[~] Encrypting {basefilename} with {password} as {resultfilename}... ', end='')
-        aes.encryptFile(basefilepath, resultfilepath, password)
+        print(f'[~] Encrypting {input_file_name} with {password} as {output_file_name}... ', end='')
+        aes.encryptFile(file_absolute_path, output_file_path, password)
     else:
-        print(f'[~] Decrypting {basefilename} with {password} as {resultfilename}... ', end='')
-        aes.decryptFile(basefilepath, resultfilepath, password)
+        print(f'[~] Decrypting {input_file_name} with {password} as {output_file_name}... ', end='')
+        aes.decryptFile(file_absolute_path, output_file_path, password)
     if delete:
-        os.remove(basefilepath)
+        os.remove(file_absolute_path)
     print('done.')
 
-def process(mode, password, filename):
-    print('[/] Pycrypt 1.0.')
-    print('[/] Mode: ' + ('encryption' if mode == 'e' else 'decryption') + ".\n[/] All files will be processed in their respective parent directory.")
-    if os.path.isdir(filename):
-        print(f'[-] {filename} is a directory, processing all sub files...')
-        process_directory(mode, password, filename)
+def process(mode, password, file_name):
+    print('[.] Pycrypt 1.0.')
+    print('[.] Mode: ' + ('encryption' if mode == 'e' else 'decryption') + ".\n[!] All files will be processed in their respective parent directory.")
+    if os.path.isdir(file_name):
+        print(f'[-] {file_name} is a directory, processing all sub files...')
+        process_directory(mode, password, os.path.abspath(file_name))
     else:
-        p = Path(filename)
-        process_file(mode, password, filename, dir=str(p.parent.absolute()))
+        process_file(mode, password, os.path.abspath(file_name))
+    print('[.] Done, exiting...')
 
-def process_directory(mode, password, dirc):
-    filenames = next(walk(dirc), (None, None, []))[2]
-    if len(filenames) == 0:
-        print(f'[!] No files found in {dirc}, exiting...')
+def process_directory(mode, password, directory):
+    files_names = next(walk(directory), (None, None, []))[2]
+    if len(files_names) == 0:
+        print(f'[!] No files found in {directory}, exiting...')
         exit(0)
     else:
-        for filename in filenames:
-            process_file(mode, password, filename, dir=dirc)
+        for file_name in files_names:
+            process_file(mode, password, os.path.abspath(file_name))
         print('[-] Directory successfully processed.')
             
 if __name__ == "__main__":
